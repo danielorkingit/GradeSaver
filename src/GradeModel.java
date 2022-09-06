5093224aab2cd652ce9965c861ed52bf4044238e
danielorkingit <danielorkin@icloud.com>
2022-09-06 21:28:33 +0200

    update

--- src/GradeModel.java
+++ src/GradeModel.java
@@ -1,29 +1,8 @@
-
 import java.io.FileWriter;
 import java.io.PrintWriter;
-
-import java.util.List;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-
-import com.google.gson.JsonParser;
-import com.google.gson.JsonElement;
-
-import org.apache.commons.io.IOUtils;
-
-import java.time.format.DateTimeFormatter;  
-import java.time.LocalDateTime;  
-
 import org.json.simple.*;
 import org.json.simple.parser.JSONParser;
-import org.mortbay.util.ajax.JSON;
-
-import com.google.gson.JsonParser;
-
 
 public class GradeModel {
 
@@ -43,6 +22,7 @@ public class GradeModel {
 		String path = "C:\\Users\\danie\\eclipse-workspace\\GradeSaver\\json\\data.json";
 		
 		try {
+			
 	        JSONParser parser = new JSONParser();
 
 			Object obj = parser.parse(new FileReader(path));
@@ -54,6 +34,7 @@ public class GradeModel {
 		    json.put("subject", subject);
 		    json.put("points", points);
 		    json.put("type", type);
+		    json.put("description", description);
 		    jsonObject.put(java.time.LocalDate.now(), json);
 		    try (PrintWriter out = new PrintWriter(new FileWriter(path))) {
 	            out.write(jsonObject.toJSONString());
--- src/Main.java
+++ src/Main.java
@@ -1,36 +1,24 @@
-import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.client.http.FileContent;
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.client.json.JsonFactory;
-import com.google.api.client.json.gson.GsonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
-import com.google.api.services.drive.DriveScopes;
 import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
-import java.io.FileNotFoundException;
+
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.List;
 import java.util.Scanner;
 
-public class Main {
-// 15jul4-Iv8TdGq8gxvH49EGX_xmNeAAls folder-id
-	  
+public class Main {	  
 
 	  public static void main(String... args) throws IOException, GeneralSecurityException {
 		  
 		Scanner userinput = new Scanner(System.in);
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		
 		String subject;
 		int type;
@@ -44,7 +32,7 @@ public class Main {
 		type = Integer.parseInt(userinput.next());
 		
 		System.out.println("Beschreibung: ");
-		description = userinput.next();
+		description = br.readLine();
 		
 		System.out.println("Punktanzahl (0-15): ");
 		points = Integer.parseInt(userinput.next());
@@ -85,18 +73,3 @@ public class Main {
 	    
 	  }
 	}
-
-
-/* public class Main {
-		
-	public static void main(String[] args) {
-		GradeModel grade = new GradeModel("Math", "Klausur", "GFS",15);
-		
-		
-		grade.writeToFile(grade.subject, grade.type, grade.description, grade.points);
-		
-		
-	}
-
-}
-*/
